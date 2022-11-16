d8504ff33db142355123992a260b0461220d381b
Danik0130 <43340726+Danik0130@users.noreply.github.com>
2022-11-16 17:33:17 +0500

    Update myapp.go

--- myapp.go
+++ myapp.go
@@ -12,6 +12,10 @@ func main() {
 	countOfFlows := flag.Int("flow", 3, "help message for flow")
 	maxNumber := flag.Int("nums", 15, "help message for nums")
 	flag.Parse()
+	if *countOfFlows <= 0 {
+		fmt.Println("Error: wrong number of flows")
+		return 
+	}
 	numberChan := make(chan int, *countOfFlows)
 	stop := make(chan bool)
 	for i := 0; i < *countOfFlows; i++ {
@@ -22,16 +26,15 @@ func main() {
 	wg.Wait()
 }
 
-// Random генерирует случайные числа в диапазоне от -x до x в бесконечном цикле, и передаёт значения в буферизированный канал
+// Random генерирует случайные числа в диапазоне от 0 до x в бесконечном цикле, и передаёт значения в буферизированный канал
 func random(maxNumber *int, numberChan chan int, stop chan bool, wg *sync.WaitGroup)  {
 	for {
-		select {
+		select {    // селектор дожидается отправки данных (закрытия канала), после чего горутины остановятся
 		case <-stop:
 			return
-		default:
-			min := -*maxNumber
+		default:  // если канал не закрыт горутины продолжают работать
 			max := *maxNumber
-			random := rand.Intn(max-min) + min
+			random := rand.Intn(max)
 			numberChan <- random
 		}
 	}
@@ -41,19 +44,19 @@ func random(maxNumber *int, numberChan chan int, stop chan bool, wg *sync.WaitGr
 /* Printer получает случайные числа из random, далее заполняет их в срез. Закрывает канал stop (завершает работу горутин), если чисел больше чем maxNumber */
 func printer(numberChan chan int, maxNumber *int, stop chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
-	numbers := make(map[int]bool)
+	numbers := make(map[int]bool) // создаём map для проверки того, что число уже попадалось
 
 	count := 0
 
 	for {
 				randomNumber := <- numberChan
-				if numbers[randomNumber] {
+				if numbers[randomNumber] { // приступаем к следующей итерации если число уже было 
 					continue
 				}
 				numbers[randomNumber] = true
 				count++
 
-		if count == *maxNumber {
+		if count == *maxNumber { /* если количество чисел = *maxNumber, то закрываем канал (завершаем гортины) и завершаем цикл */
 			close(stop)
 			break
 		}
