ca34dd4f045fb61cb57312c8510cdf264c8ac4ef
Danik0130 <43340726+Danik0130@users.noreply.github.com>
2022-11-17 16:33:00 +0500

    Update main.js

--- generator/static/main.js
+++ generator/static/main.js
@@ -2,26 +2,28 @@ console.log("JS Loaded")
 
 const url = "127.0.0.1:5555"
 
-var inputForm = document.getElementById("inputForm")
+var inputForm = document.getElementById("inputForm") // получаем форму по айди
 
-inputForm.addEventListener("submit", (e) => {
+inputForm.addEventListener("submit", (e) => { // при нажатии кнопки запускаем передачу данных методом POST
 
-    //prevent auto submission
-    e.preventDefault()
+  // предотвращаем автоматическую оправку
+  e.preventDefault()
 
-    const formdata = new FormData(inputForm)
-    fetch(url, {
+  const formdata = new FormData(inputForm)
+  fetch(url, {
 
-        method: "POST",
-        body: formdata,
-    }).then(
-        response => response.text()
-    ).then(
-        (data) => { console.log(data);
-            document.getElementById("serverMessageBox").innerHTML = data }
-    ).catch(
-        error => console.error(error)
-    )
+    method: "POST",
+    body: formdata,
+  }).then(
+    response => response.text() 
+  ).then(
+    (data) => { // отправили ответ с сервера
+      console.log(data);
+      document.getElementById("serverMessageBox").innerHTML = data
+    }
+  ).catch(
+    error => console.error(error)
+  )
 
 
 
